3004e4e2710bce75e7114e31019b751244de3f24
Mito Youhei <mitoyouhei@proton.me>
2024-06-16 18:27:46 +0800

    enable local register and login

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,30 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const session = require("express-session");
 const cors = require("cors");
 const app = express();
 const tombstones = require("./routes/tombstones");
+const passport = require("./passport");
+const authRouter = require("./routes/auth");
 const PORT = process.env.PORT || 5001;
+
+const SESSION_SECRET = process.env.SESSION_SECRET || "your_session_secret";
 
 app.use(cors());
 app.use(express.json());
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/api/tombstones", tombstones);
+app.use("/api/auth", authRouter);
 
 mongoose
   .connect(process.env.DATABASE_URL, {
